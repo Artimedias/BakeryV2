eefd829ddb647ccda4bddb80b7956b644c5eb163
Artimedias <brettroper22@gmail.com>
2022-12-04 05:50:21 -0800

    trying to add the tests

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace BreadCalc
+{
+    public class Bread
+    {
+        public int BreadPrice(int food, int price)
+        {      
+            while (food > 0)
+            { 
+                if (food >= 3)
+                {
+                    if((food % 3) == 0)
+                    {
+                        price += ((food / 3) * 10);
+                        food = 0;  
+                    }
+                    else
+                    {
+                        price = price + (((food -(food % 3)) / 3) * 10);
+                        food = (food % 3);
+                    }
+                }
+                else
+                {
+                    price = price + (food * 5);
+                    food = 0;
+                }
+            }
+            return price;
+        }
+    }
+}
---
+++ Bakery/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BreadCalc;
+using PastryCalc;
+
+
+namespace Bakery
+{
+  public class BakeryFront
+  {
+    public string TakeOrder()
+    {
+      /*char[] strArr = new char[str.Length];
+      //breaks it into an array of letters
+      for(int i = 0; i < str.Length; i++)
+      {
+        strArr[i] = str[i];
+        Console.WriteLine(str[i]);
+      }
+      //smushes it back into a string
+      Array.Reverse(strArr);
+      var reversedString = new string (strArr);
+      
+      if(reversedString == str)
+      {
+        return true;
+      }*/
+      return "na";
+    }
+
+    public static void Main()
+    {
+      /*Console.WriteLine("Enter in a palindrome: ");
+      string userInput = Console.ReadLine();
+      //PalindromeChecker pal1 = new PalindromeChecker();
+
+      if (pal1.IsPalindromeString(userInput))
+      {
+        Console.WriteLine("WOOOHOOO!!!! " + userInput + " is a palindrome!!!!!!!!!!!!");
+      }
+      else
+      {
+        Console.WriteLine("Read a palindrome dictionary!");
+      }*/
+    }
+  }
+}
